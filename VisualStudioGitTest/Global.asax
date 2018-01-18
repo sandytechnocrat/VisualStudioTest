5e5cff016bdd9b2ed40bfbf33d75c031c441d44f
Sandeep <sandeep.telang@gmail.com>
2018-01-18 22:57:38 +0530

    Initial

---
+++ VisualStudioGitTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VisualStudioGitTest.MvcApplication" Language="C#" %>
---
+++ VisualStudioGitTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VisualStudioGitTest.Startup))]
+namespace VisualStudioGitTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
